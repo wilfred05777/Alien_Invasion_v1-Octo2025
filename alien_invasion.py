2c63eae7013c01a589c450b2a6a8a3a01917986a
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-10-15 18:49:26 +0800

    di mo gawas ang image

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -16,6 +17,9 @@ class AlienInvasion:
         # self.screen = pygame.display.set_mode((1200, 800))
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
         # Set the background color.
         self.bg_color = (230, 230, 230)
 
