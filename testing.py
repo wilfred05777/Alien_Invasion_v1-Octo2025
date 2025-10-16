7b1f553ca8676506e6ed4b49193b8d592c6ad944
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-10-16 18:41:43 +0800

    addition is the wrong and not calling the correct function name like here in my case addtion

---
+++ testing.py
@@ -0,0 +1,9 @@
+def addition(first_value, second_value):
+    return (first_value + second_value)
+    
+first_value = int(input("Enter first number:"))
+second_value = int(input("Enter second number:"))
+
+# print(f"Sum of {first_value} and {second_value} is {sum(first_value, second_value)}")
+
+print(f"Sum of {first_value} and {second_value} is {addition(first_value, second_value)}")
