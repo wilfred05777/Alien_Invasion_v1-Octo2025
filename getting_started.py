dd3c506e8d262752a014eb479bc6a7f4495701cc
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-10-17 11:28:57 +0800

    getting start

---
+++ getting_started.py
@@ -0,0 +1,10 @@
+# https://learn.online-python.com/python/what-is-python
+
+print("Welcome to the Getting Started Guide!")
+
+name = "Developer User"
+print(f"Hello, {name}!")
+
+
+number = [2, 4, 6, 8, 10]
+print(f"Here is a list of the numbers {number}")
